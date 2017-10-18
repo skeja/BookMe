18a738fa1dce2a1aee8a0d979eba30f54581a14d
Ante Skejić <ante.skejic@gmail.com>
2017-10-18 20:45:16 +0200

    Apply new code style

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const app = express();
 const mongoose = require("mongoose");
 
 const indexRoutes = require('./routes');
- 
+
 mongoose.connect('mongodb://localhost/bookme',{ useMongoClient: true });
 
 app.use(indexRoutes);
- 
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('App running on port 3000!');
 });
