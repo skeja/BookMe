40d7efe890734f7ddffdc8a7d41869789fe6142d
Ante Skejić <ante.skejic@gmail.com>
2017-10-30 15:12:50 +0100

    Booking model updated

--- models/booking.js
+++ models/booking.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
-
+  author: String,
+  startDate: Date,
+  endDate: Date,
+  price: Number,
+  origin: String,
+  guests: Number,
+  nation: String
 });
 
 const booking = mongoose.model('Booking', bookingSchema);
