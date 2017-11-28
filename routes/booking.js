58e487cf70dc17434bd3f4974ab9e4afdbb40c43
Ante Skejić <ante.skejic@gmail.com>
2017-11-28 14:39:25 +0100

    Minor fix

--- routes/booking.js
+++ routes/booking.js
@@ -4,13 +4,13 @@ const { Booking } = require('./db');
 
 function create(req, res, next) {
    return Booking.create(req.body)
-    .then(newBooking => res.send(newBooking))
+    .then(booking => res.send(booking))
     .catch(err => next(err));
   }
 
 function list(req, res, next) {
   return Booking.find()
-    .then(allBookings => res.send(allBookings))
+    .then(bookings => res.send(bookings))
     .catch(err => next(err));
 };
 
@@ -32,6 +32,7 @@ function remove(req, res, next) {
   return Booking.findByIdAndRemove(req.params.id)
     .catch(err => next(err));
 };
+
 router.get('/booking', list);
 router.post('/bookig', create);
 router.get('/booking/:id', find);
