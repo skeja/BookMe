e1ed5a8c87528251564dc22033b2f7f2e56428d4
Ante Skejić <ante.skejic@gmail.com>
2017-12-07 15:23:45 +0100

    Added "Not found" handler

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.use((err, req, res, next) => {
   res.status(500).send(err.message);
 });
 
+app.use((req, res, next) => res.sendStatus(404));
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('App running on port 3000!');
 });
